ce058c54e7e0aa39309066cd89fa19e97559d9d2
devmdtanvir <officialtanvir@hotmail.com>
2023-01-05 12:06:16 +0600

    I am added another file

--- index.js
+++ index.js
@@ -10,14 +10,22 @@ const friends = [
     { name: 'Shazzed', roll: 30, section: 'B' },
     { name: 'Rymon', roll: 30, section: 'B' }
 ]
+const names = ['Tanvir', 'Shakil', 'Tamim', 'Sabbir']
 
 app.get('/', (req, res) => {
     res.send("Yay i am running for node fermwork")
 })
 
 // another get request 
+app.get('/name/:id', (req, res) => {
+    const id = req.params.id
+    const name = names[id]
+    res.send({ name, id });
+    console.log(req.query)
+})
 app.get('/user', (req, res) => {
     res.send(friends);
 })
+
 
 app.listen(port, () => console.log(`lisiting port ${port}`))
